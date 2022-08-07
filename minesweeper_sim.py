f4e548423fb00792a8d76ba0ca520078d6177b39
Games Computers Play <87609129+gamescomputersplay@users.noreply.github.com>
2022-08-07 17:23:56 +0800

    Support N dimenstion (2-4)

--- minesweeper_sim.py
+++ minesweeper_sim.py
@@ -1,5 +1,6 @@
-''' Class for simulating minesweeper games:
+''' Class for simulating n-dimensional minesweeper games:
 generte board, input a move, answer with resulting boards etc.
+(Visualization only works for n in (2, 3, 4))
 '''
 
 import random
@@ -13,16 +14,17 @@ class GameSettings:
     ''' Class to hold game settings:
     widyjs and height of teh board, number of mines
     '''
-    width: int = 8
-    height: int = 8
+    size: tuple = (8, 8)
     mines: int = 10
 
 
 # Presets for main game sizes
-GAME_BEGINNER = GameSettings(8, 8, 10)
-GAME_INTERMEDIATE = GameSettings(16, 16, 40)
-GAME_EXPERT = GameSettings(30, 16, 99)
-GAME_TEST = GameSettings(5, 4, 4)
+GAME_BEGINNER = GameSettings((8, 8), 10)
+GAME_INTERMEDIATE = GameSettings((16, 16), 40)
+GAME_EXPERT = GameSettings((30, 16), 99)
+GAME_TEST = GameSettings((5, 4), 4)
+GAME_3D = GameSettings((3, 5, 4), 7)
+GAME_4D = GameSettings((4, 3, 6, 5), 10)
 
 # Cell types
 CELL_MINE = -1
@@ -47,40 +49,74 @@ class MinesweeperGame:
         '''
 
         # Unpack game parameters
-        self.width = settings.width
-        self.height = settings.height
-        # Make sure there no more mines than cells
-        self.mines = min(settings.mines, self.width * self.height)
+        self.shape = settings.size
+        self.dimensions = len(self.shape)
 
+        # Make sure there no more mines than cells minus one
+        self.mines = min(settings.mines, np.prod(self.shape) - 1)
+
+        # Counter of remaining mines (according to what plays
+        # marked as mines, can be inaccurate).
         self.remaining_mines = self.mines
 
-        # Use random seed, if passed
+        # This is just a dict to store all the neighbouring coordinates
+        # for all cells, so we won't have to recalculate them every move
+        self.neighbour_buffer = {}
+
+        # Use random seed, if seed passed
         if seed is not None:
             random.seed(seed)
 
         # Generate field: array of mines and numbers that player
         # cannot see yet
         self.field = self.generate_mines()
+
         # Populate it with numbers
         self.generate_numbers()
 
         # Initiate "uncovered": the part of the field
         # that player sees
-        self.uncovered = np.full((self.width, self.height), CELL_COVERED)
+        self.uncovered = np.full(self.shape, CELL_COVERED)
 
         # Default status
         self.status = STATUS_ALIVE
 
     def iterate_over_all_cells(self):
-        ''' Returns a list [(0,0), (0,1) .. (width-1, height-1)]
-        kind of like "range" but for 2d array of cells.
+        ''' Returns a list
+        [(0, .., 0), (0, .., 1) .. (d1-1, .., dn-1)]
+        of all possible coordinates.
+        Kind of like "range" but for D-dimensional array of cells.
         '''
         permutations = []
-        # Iterate over self.height, so it would go line after line,
-        # not column after column
-        for j in range(self.height):
-            for i in range(self.width):
-                permutations.append((i, j))
+
+        # We'll have as many permutations as product of all dimensions
+        for permutation_number in range(np.prod(self.shape)):
+
+            # List to construct a permutation in
+            this_permutation = []
+            # And a copy of permutation's cardinal number
+            # (need a copy, as we will mutate  it)
+            remainding_number = permutation_number
+
+            # Go from back to front through the dimension
+            for pos in reversed(self.shape):
+
+                # It is somewhat similar to base conversion,
+                # except base changes for each place.
+
+                # This permutation is just a remainder of division
+                this_permutation.append(remainding_number % pos)
+                # But you need to make sure you substract by
+                # the number in the latest position
+                remainding_number -= this_permutation[-1]
+                # and divide by the latest base
+                remainding_number //= pos
+
+            # Reverse the resulting list (as we started from the right side,
+            # the smallest digits), and store it in the final list
+            this_permutation.reverse()
+            permutations.append(tuple(this_permutation))
+
         return permutations
 
     def valid_coords(self, cell):
@@ -88,43 +124,62 @@ class MinesweeperGame:
         (do't go over field's size).
         Return Tru / False
         '''
-        coord_x, coord_y = cell
-        if 0 <= coord_x < self.width and 0 <= coord_y < self.height:
-            return True
-        return False
+        for i, dimension_size in enumerate(self.shape):
+            if cell[i] < 0 or cell[i] >= dimension_size:
+                return False
+        return True
 
     def cell_surroundings(self, cell):
-        ''' Returns a list of neighbours of a cell coord_x, coord_y,
+        ''' Returns a list of coordinates of neighbours of a cell
         taking borders into account
         '''
-        surroundings = []
-        coord_x, coord_y = cell
-        # Iterate over 3x3
-        for i in (-1, 0, 1):
-            for j in (-1, 0, 1):
+        # Dinamic programming: use buffer if the result is there
+        if cell in self.neighbour_buffer:
+            return self.neighbour_buffer[cell]
 
-                # Not the cell itself
-                if (i, j) != (0, 0):
-                    # And is not outside the border
-                    if self.valid_coords((coord_x + i, coord_y + j)):
-                        surroundings.append((coord_x + i, coord_y + j))
+        surroundings = []
+
+        # This is to calculate offset. But done outside of for
+        # as this is the same for all iterations
+        powers = {j: 3**j for j in range(len(self.shape))}
+
+        # Iterate over 3 ** 'N of dimensions' postential neighbours
+        for i in range(3 ** len(self.shape)):
+
+            # Way to calculate all (1, -1, 0) for this permutation
+            offset = tuple((i // powers[j]) % 3 - 1
+                           for j in range(len(self.shape)))
+
+            # If it is the cell itself - skip
+            if offset.count(1) == 0 and offset.count(-1) == 0:
+                continue
+
+            # If resulting coords are valid
+            cell_with_offest = tuple([cell[i] + offset[i]
+                                      for i in range(len(self.shape))])
+            if self.valid_coords(cell_with_offest):
+                surroundings.append(cell_with_offest)
+
+        # Store in buffer, for future reuse
+        self.neighbour_buffer[cell] = surroundings
 
         return surroundings
 
     def random_coords(self):
-        ''' Generate a pair of random coordinates
-        within field's size. Returns a tuple
+        ''' Generates a tuple of self.size random coordinates
+        each within the appropriate dimension's size. Returns a tuple
         '''
-        random_x = random.randint(0, self.width - 1)
-        random_y = random.randint(0, self.height - 1)
-        return (random_x, random_y)
+        coordinates = []
+        for dimension_size in self.shape:
+            coordinates.append(random.randint(0, dimension_size - 1))
+        return tuple(coordinates)
 
     def generate_mines(self):
         '''Generate a game field (np array) with this size and mines
         according to the setting. Mines are marked as -1
         '''
-        # Start with an empty NP array
-        field = np.full((self.width, self.height), 0)
+        # Start with an 0-filled NP array
+        field = np.full(self.shape, 0)
 
         # Set all mines
         for _ in range(self.mines):
@@ -156,8 +211,8 @@ class MinesweeperGame:
 
             # Iteration over neighbours of a mine
             # Add 1 for any mine neighbour
-            for (neib_x, neib_y) in self.cell_surroundings(cell):
-                if self.field[neib_x, neib_y] == CELL_MINE:
+            for neighbour in self.cell_surroundings(cell):
+                if self.field[neighbour] == CELL_MINE:
                     self.field[cell] += 1
 
     def has_covered(self):
@@ -321,10 +376,13 @@ class MinesweeperGame:
 
         return self.status
 
-    def field2str(self, field_to_show, show_ruler=True):
-        ''' Visual representation of the board
-        (Can be used for both secret and uncovered boards)
+    @staticmethod
+    def field2str_2d(field_to_show, show_ruler=True):
+        ''' Visual representation of the 2D field
         '''
+        height = field_to_show.shape[1]
+        width = field_to_show.shape[0]
+
         # Characters to show for different statuses
         characters_to_display = {**{
             CELL_MINE: "*",
@@ -344,22 +402,22 @@ class MinesweeperGame:
             # but both digits for those ending in 0 etc
             output += "".join([f"{i}" if i % 10 == 0 and i != 0
                                else f"{i % 10} "
-                               for i in range(self.width)])
-            output += "\n"
+                               for i in range(width)])
+            output += " \n"
             output += " " * 3
 
         # Top border
-        output += "-" * (self.width * 2 + 3) + "\n"
+        output += "-" * (width * 2 + 3) + "\n"
 
         # Iterate over all cells
-        for row in range(self.height):
+        for row in range(height):
 
             # Left border
             if show_ruler:
                 output += f"{row:2} "
             output += "! "
 
-            for col in range(self.width):
+            for col in range(width):
 
                 cell_value = field_to_show[col, row]
 
@@ -372,9 +430,74 @@ class MinesweeperGame:
         # Bottom border
         if show_ruler:
             output += " " * 3
-        output += "-" * (self.width * 2 + 3) + "\n"
+        output += "-" * (width * 2 + 3) + "\n"
 
         return output
+
+    def field2str_3d(self, field_to_show, show_ruler=True):
+        ''' Visual representation of the 3D field
+        '''
+        depth = field_to_show.shape[0]
+        buffer = []
+        # Slice 3D array into 2D arrays
+        # And get strings for each one into a buffer
+        for i in range(depth):
+            buffer.append(self.field2str_2d(field_to_show[i])
+                          .split("\n"))
+
+        output = ""
+
+        # Horizontal rule numbering each 2D field
+        if show_ruler:
+            # Pad it, so the number is in the middle of a field
+            field_with = len(buffer[0][0])
+            padding_left = " " * ((field_with - 2) // 2 + 2)
+            padding_right = " " * (field_with - len(padding_left) - 1)
+            for field_n, _ in enumerate(buffer):
+                output += padding_left + str(field_n) + padding_right
+            output += "\n"
+
+        # Iterate over each line in the buffer.
+        # Sort of "transpose" it
+        for line_n in range(len(buffer[0])):
+            for field in buffer:
+                output += field[line_n]
+            # We don't need the very last new line
+            if line_n != len(buffer[0]) - 1:
+                output += "\n"
+
+        return output
+
+    def field2str_4d(self, field_to_show, show_ruler=True):
+        ''' Visual representation of the 4D field
+        '''
+        fourth = field_to_show.shape[0]
+        output = ""
+        for i in range(fourth):
+            show_ruler = True if i == 0 else False
+            fields = self.field2str_3d(field_to_show[i],
+                                       show_ruler=show_ruler)
+            cur_line_of_fields = fields.split("\n")[:-1]
+            for line_n, line in enumerate(cur_line_of_fields):
+                if line_n == len(cur_line_of_fields) // 2 + \
+                   (1 if i == 0 else 0):
+                    output += str(i)
+                else:
+                    output += " "
+                output += line + "\n"
+        return output
+
+    def field2str(self, field_to_show, show_ruler=True):
+        ''' Pick the right string representation function:
+        works for 2 to 4 D minesweeper fields
+        '''
+        if len(field_to_show.shape) == 2:
+            return self.field2str_2d(field_to_show, show_ruler)
+        if len(field_to_show.shape) == 3:
+            return self.field2str_3d(field_to_show, show_ruler)
+        if len(field_to_show.shape) == 4:
+            return self.field2str_4d(field_to_show, show_ruler)
+        return "Can't display the field, sorry"
 
     def __str__(self):
         ''' Display uncovered part of the board
@@ -390,31 +513,30 @@ class MinesweeperGame:
         returns two lists that can be fed right into "make_a_move"
         '''
         safe, mines = [], []
-        mode, coord_x, coord_y = None, None, None
+        mode, cell = None, []
 
         # Break th estring by spaces
         for chunk in string.upper().split(" "):
 
             # We have a coordinate
             if chunk.isnumeric():
-                # First one must be an X, second an Y
-                if coord_x is None:
-                    coord_x = int(chunk)
-                else:
-                    coord_y = int(chunk)
-                    cell = (coord_x, coord_y)
+                # Add it to the coordinates list
+                cell.append(int(chunk))
+
+                # if coords is long enough
+                if len(cell) == len(self.shape):
 
                     # Append the lists accordingly
                     # Single mine
                     if mode == "M":
-                        mines.append(cell)
+                        mines.append(tuple(cell))
                     # Open around, add all surroudings
                     elif mode == "A":
                         safe.extend(self.cell_surroundings(cell))
                     # Single safe
                     else:
-                        safe.append(cell)
-                    mode, coord_x, coord_y = False, None, None
+                        safe.append(tuple(cell))
+                    mode, cell = False, []
 
             # M and A modify the behaviour
             elif chunk in ("M", "A"):
@@ -427,7 +549,7 @@ def main():
     ''' Some tests for minesweeper sim
     '''
 
-    # Random seed to generatethe game
+    # Seed to generatethe game (None for random)
     seed = None
 
     status_message = {STATUS_ALIVE: "Still alive",
@@ -453,6 +575,7 @@ def main():
         print(f"Remaining mines: {game.remaining_mines}")
 
     print("Thanks you for playing!")
+
 
 if __name__ == "__main__":
     main()
