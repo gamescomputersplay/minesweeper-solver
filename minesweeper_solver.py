7ee2b29b42f0473d8e154a4fbc919a4838744b28
gamescomputersplay <gamescomputersplay@gmail.com>
2022-08-09 18:45:10 +0800

    Refactor "MineGroups" logic into separate class

--- minesweeper_solver.py
+++ minesweeper_solver.py
@@ -8,17 +8,22 @@ import minesweeper_game as ms
 
 
 class MineGroup:
-    ''' A minegroup is a set of cells that are know
+    ''' A minegroup is a set of cells that are known
     to have a certian number of mines.
-    The basic element for many solving methods
+    For example "cell1, cell2, cell3 have exactly 1 mine" or
+    "cell4, cell5 have at least 1 mine".
+    This is a basic element for Groups and Subgroups solving methods.
     '''
 
-    def __init__(self, cells, mines):
+    def __init__(self, cells, mines, group_type="exactly"):
         ''' Cells in questions. Number of mines that those cells have.
         '''
         # Use set rather than list as it speeds up some calculations
         self.cells = set(cells)
         self.mines = mines
+
+        # Group type (exctly, no more than, no fewer than)
+        self.group_type = group_type
 
         # Calculate hash (for dedupliaction)
         self.hash = self.calculate_hash()
@@ -37,19 +42,67 @@ class MineGroup:
         ''' Hash of a group. To check if such group is already in self.group
         '''
         # Prepare data for hashing: sort cells, add self.mines
-        for_hash = sorted(list(self.cells)) + [self.mines]
+        for_hash = sorted(list(self.cells)) + [self.mines] + [self.group_type]
         # Make immutable
         for_hash = tuple(for_hash)
         # Return hash
         return hash(for_hash)
 
     def __str__(self):
-        ''' List cells and mines
+        ''' List cells, mines and group type
         '''
         out = "Cell(s) "
         out += ",".join([str(cell) for cell in self.cells])
-        out += f" have {self.mines} mines"
+        out += f" have {self.group_type} {self.mines} mines"
         return out
+
+
+class AllMineGroups:
+    ''' Functions to handle a group of MineGroup object:
+    deduplicate them, generate subgroups ("at least" and "no fewer than")
+    '''
+
+    def __init__(self):
+        # Hashes for deduplication
+        self.hashes = set()
+        # List of minegroups
+        self.mine_groups = []
+
+    def reset(self):
+        ''' Clear the data
+        '''
+        self.hashes = set()
+        self.mine_groups = []
+
+    def add(self, new_group):
+        ''' Check if if have this group already.
+        If not, add to the list
+        '''
+        if new_group.hash not in self.hashes:
+            self.mine_groups.append(new_group)
+            self.hashes.add(new_group.hash)
+
+    def __iter__(self):
+        ''' For iterator, use the list of groups
+        '''
+        return iter(self.mine_groups)
+
+    def generate_subgroups(self):
+        ''' Populate self.subgroups ("no more than", "no fewer than"
+        groups, derived from groups).
+        '''
+
+        # Generate "at least" subgroup
+        for group in self:
+            if len(group.cells) > group.mines and len(group.cells) > 2 and \
+               group.mines > 1 and group.group_type in ("exactly", "at least"):
+                print("G", group)
+                for cell in group.cells:
+
+                    new_subgroup = MineGroup(group.cells.difference({cell}),
+                                             group.mines - 1, "at least")
+                    self.add(new_subgroup)
+                    print("SG", new_subgroup)
 
 
 class MinesweeperSolver:
@@ -71,9 +124,7 @@ class MinesweeperSolver:
         self.field = None
 
         # Placeholder for all groups. Recalculated for each solver run
-        self.groups = []
-        # Hashes for all groups, for deduplication.
-        self.groups_hashes = set()
+        self.groups = AllMineGroups()
 
     def get_all_covered(self):
         ''' Return the list of all covered cells
@@ -95,8 +146,7 @@ class MinesweeperSolver:
         '''Populate self.group with all found cell groups
         '''
         # Reset groups and hashes
-        self.groups = []
-        self.groups_hashes = set()
+        self.groups.reset()
 
         # Go over all cells and find all the "Numbered ones"
         for cell in self.helper.iterate_over_all_cells():
@@ -122,10 +172,7 @@ class MinesweeperSolver:
                 # store it in the self.groups
                 if covered_neighbours:
                     new_group = MineGroup(covered_neighbours, active_mines)
-                    # Check if don't have the same group already
-                    if new_group.hash not in self.groups_hashes:
-                        self.groups.append(new_group)
-                        self.groups_hashes.add(new_group.hash)
+                    self.groups.add(new_group)
 
     def method_naive(self):
         ''' Method #1. Naive
@@ -181,11 +228,15 @@ class MinesweeperSolver:
                     else:
                         new_group = MineGroup(group_b.cells - group_a.cells,
                                               group_b.mines - group_a.mines)
-                        # If it is a new group, add it to the current list
-                        if new_group.hash not in self.groups_hashes:
-                            self.groups.append(new_group)
-                            self.groups_hashes.add(new_group.hash)
+                        self.groups.add(new_group)
 
+        return list(set(safe)), list(set(mines))
+
+    def method_subgroups(self):
+        ''' Subgroups method. Based on breaking groups down "into no more
+        than", "no fewer than" subgroups and cross checking them with groups.
+        '''
+        safe, mines = [], []
         return list(set(safe)), list(set(mines))
 
     def solve(self, field):
@@ -202,7 +253,7 @@ class MinesweeperSolver:
 
         # 0. First click on the "all 0" corner
         if self.helper.are_all_covered(self.field):
-            return [tuple(0 for _ in range(len(self.shape))),], None
+            return [tuple(0 for _ in range(len(self.shape))), ], None
 
         # Generate groups (main data for basic solving methods)
         self.generate_groups()
@@ -219,6 +270,13 @@ class MinesweeperSolver:
         if safe or mines:
             return safe, mines
 
+        # 3. Sub groups Method
+        ######################
+        # self.groups.generate_subgroups()
+        safe, mines = self.method_subgroups()
+        if safe or mines:
+            return safe, mines
+
         # If there is nothing we can do: return a random cell
         return [self.pick_a_random_cell(covered_cells), ], None
 
@@ -228,8 +286,9 @@ def main():
     '''
 
     settings = ms.GAME_TEST
+    settings = ms.GAME_BEGINNER
 
-    game = ms.MinesweeperGame(settings)
+    game = ms.MinesweeperGame(settings, seed=0)
     solver = MinesweeperSolver(settings)
 
     while game.status == ms.STATUS_ALIVE:
