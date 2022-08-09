b16641018fa520c6c4c8b16b54cf33dd28a7882e
gamescomputersplay <gamescomputersplay@gmail.com>
2022-08-09 14:18:06 +0800

    Added "Groups" solving method

--- minesweeper_solver.py
+++ minesweeper_solver.py
@@ -20,6 +20,9 @@ class MineGroup:
         self.cells = set(cells)
         self.mines = mines
 
+        # Calculate hash (for dedupliaction)
+        self.hash = self.calculate_hash()
+
     def is_all_safe(self):
         ''' If group has 0 mines, it is safe
         '''
@@ -29,6 +32,16 @@ class MineGroup:
         ''' If group has as many cellas mines - they are all mines
         '''
         return len(self.cells) == self.mines
+
+    def calculate_hash(self):
+        ''' Hash of a group. To check if such group is already in self.group
+        '''
+        # Prepare data for hashing: sort cells, add self.mines
+        for_hash = sorted(list(self.cells)) + [self.mines]
+        # Make immutable
+        for_hash = tuple(for_hash)
+        # Return hash
+        return hash(for_hash)
 
     def __str__(self):
         ''' List cells and mines
@@ -59,6 +72,8 @@ class MinesweeperSolver:
 
         # Placeholder for all groups. Recalculated for each solver run
         self.groups = []
+        # Hashes for all groups, for deduplication.
+        self.groups_hashes = set()
 
     def get_all_covered(self):
         ''' Return the list of all covered cells
@@ -79,7 +94,9 @@ class MinesweeperSolver:
     def generate_groups(self):
         '''Populate self.group with all found cell groups
         '''
+        # Reset groups and hashes
         self.groups = []
+        self.groups_hashes = set()
 
         # Go over all cells and find all the "Numbered ones"
         for cell in self.helper.iterate_over_all_cells():
@@ -105,19 +122,70 @@ class MinesweeperSolver:
                 # store it in the self.groups
                 if covered_neighbours:
                     new_group = MineGroup(covered_neighbours, active_mines)
-                    self.groups.append(new_group)
+                    # Check if don't have the same group already
+                    if new_group.hash not in self.groups_hashes:
+                        self.groups.append(new_group)
+                        self.groups_hashes.add(new_group.hash)
 
-    def naive_method(self):
+    def method_naive(self):
         ''' Method #1. Naive
         Check if there are all safe or all mines groups.
         Return safe and mines found in those groups
         '''
         safe, mines = [], []
         for group in self.groups:
+
+            # No remaining mines
             if group.is_all_safe():
                 safe.extend(list(group.cells))
+
+            # All covered are mines
             if group.is_all_mines():
                 mines.extend(list(group.cells))
+
+        return list(set(safe)), list(set(mines))
+
+    def method_groups(self):
+        ''' Method #2. Groups
+        Cross check all groups. When group is a subset of
+        another group, try to deduce safe or mines.
+        '''
+        safe, mines = [], []
+
+        # Cross-check all-withall groups
+        for group_a in self.groups:
+            for group_b in self.groups:
+
+                # Don't compare with itself
+                if group_a.hash == group_b.hash:
+                    continue
+
+                # See if A is a subset of B
+                # Note: we assume that B is bigger, becase when we add extra
+                # groups (in the end of this method), they are guaranteed to
+                # be be compared as group_a to all items as group_b. As the new
+                # item will be smaller, group_b item will be a bigger one
+                if group_a.cells.issubset(group_b.cells):
+
+                    # If they have the same mines: difference is safe
+                    if group_b.mines == group_a.mines:
+                        safe.extend(list(group_b.cells - group_a.cells))
+
+                    # If difference in number of cells is the same as
+                    # difference in number of mines: difference is mines
+                    elif len(group_b.cells - group_a.cells) == \
+                         group_b.mines - group_a.mines:
+                        mines.extend(list(group_b.cells - group_a.cells))
+
+                    # Difference in cells and mines can also become a new group
+                    else:
+                        new_group = MineGroup(group_b.cells - group_a.cells,
+                                              group_b.mines - group_a.mines)
+                        # If it is a new group, add it to the current list
+                        if new_group.hash not in self.groups_hashes:
+                            self.groups.append(new_group)
+                            self.groups_hashes.add(new_group.hash)
+
         return list(set(safe)), list(set(mines))
 
     def solve(self, field):
@@ -132,15 +200,22 @@ class MinesweeperSolver:
 
         covered_cells = self.get_all_covered()
 
-        # if it is a very first move: pick a random cell
+        # 0. Random first click
         if self.helper.are_all_covered(self.field):
             return [self.pick_a_random_cell(covered_cells), ], None
 
         # Generate groups (main data for basic solving methods)
         self.generate_groups()
 
-        # Try naive method. If it worked, return safe and mines lists
-        safe, mines = self.naive_method()
+        # 1. Naive Method
+        #################
+        safe, mines = self.method_naive()
+        if safe or mines:
+            return safe, mines
+
+        # 2. Groups Method
+        ##################
+        safe, mines = self.method_groups()
         if safe or mines:
             return safe, mines
 
@@ -160,11 +235,11 @@ def main():
     while game.status == ms.STATUS_ALIVE:
 
         safe, mines = solver.solve(game.uncovered)
-        print(safe, mines)
+        print(f"Move: Safe {safe}, Mines {mines}")
         game.make_a_move(safe, mines)
         print(game)
 
-    print(game.status)
+    print(ms.STATUS_MESSAGES[game.status])
 
 
 if __name__ == "__main__":
