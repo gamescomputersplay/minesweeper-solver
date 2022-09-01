c86d016f3ccd89dd90eab11a8f556a757be18efe
gamescomputersplay <gamescomputersplay@gmail.com>
2022-09-01 18:31:02 +0800

    Bot: finding the field (2)

--- minesweeper_bot.py
+++ minesweeper_bot.py
@@ -3,10 +3,12 @@ field on the screen, recognizing the numbers, clicking cells etc.
 Uses minesweeper solver in minesweeper_solver.py
 '''
 
-#import pyautogui
+# import pyautogui
+import time
 from PIL import Image, ImageDraw
 
 MINIMUM_CELL_SIZE = 10
+
 
 def find_game(image, colors):
     '''Find game field by looking for squares of color "colors",
@@ -35,6 +37,8 @@ def find_game(image, colors):
         # Check if all the pixels are of the needed color
         for i in range(left, right + 1):
             for j in range(top, bottom + 1):
+
+                # This is not a one-color square
                 if pixels[i, j] != color:
                     return False, False, False, False
 
@@ -62,9 +66,48 @@ def find_game(image, colors):
                         found.append((left, top, right, bottom))
 
                         # Fill it with black so it would not be found again
-                        draw.rectangle((left, top, right, bottom), fill=(0, 0, 0))
+                        draw.rectangle((left, top, right, bottom),
+                                       fill=(0, 0, 0))
+                    else:
+                        # Paint it over, so we will not have to test
+                        # these pixels again
+                        draw.line((left, top, right, top), fill=(0, 0, 0))
+                        draw.line((left, top, left, bottom), fill=(0, 0, 0))
 
         return found
+
+    def filter_grid(found):
+        ''' Given found squares, only keep those that are on a "grid":
+        their coordinates are the most repeating coordinates int the list
+        '''
+        # Count all x and y coordinates of all the squares we found
+        x_count, y_count = {}, {}
+        for left, top, right, bottom in found:
+            x_count[left] = x_count.get(left, 0) + 1
+            y_count[top] = y_count.get(top, 0) + 1
+            x_count[right] = x_count.get(right, 0) + 1
+            y_count[bottom] = y_count.get(bottom, 0) + 1
+
+        # Calculate "weight" - how often this squares coordinates are present
+        # in other squares
+        found_with_weights = {}
+        all_weights = []
+        for left, top, right, bottom in found:
+            weight = x_count[left] + y_count[top] + \
+                     x_count[right] + y_count[bottom]
+            found_with_weights[(left, top, right, bottom)] = weight
+            all_weights.append(weight)
+
+        # Find median of all weights. Anything higher or equal to than
+        # will be in the final grid
+        all_weights.sort()
+        threshold = all_weights[len(all_weights) // 2]
+
+        new_found = [coordinates
+                     for coordinates, weight in found_with_weights.items()
+                     if weight >= threshold]
+
+        return new_found
 
     # Pixels of the input image
     pixels = image.load()
@@ -76,10 +119,13 @@ def find_game(image, colors):
     found = find_all_squares()
 
     # Filter those that are on the same grid
+    found = filter_grid(found)
 
     # Sort them into rows and columns
 
-    print (found)
+    print(len(found))
+    # print(found)
+
 
 def main():
     ''' Some testing functions
@@ -90,5 +136,8 @@ def main():
     find_game(screenshot, colors)
 
 
-if  __name__ == "__main__":
-    main()
+if __name__ == "__main__":
+    start = time.time()
+    for _ in range(10):
+        main()
+    print(time.time() - start)
